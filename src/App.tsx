b4d6809e9620a5bf8b7294703ccfc9f4f197ac29
Aigars <aigarspeda@gmail.com>
2020-06-10 10:31:52 +0300

    started to setting up router

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,21 @@
 import React from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+
+import Login from "./components/auth/login/Login";
+import Register from "./components/auth/register/Register";
 
 class App extends React.PureComponent {
   render() {
     return (
-      <div>
-        <h1>Ticket App</h1>
-      </div>
+      <Router>
+        <div className="App container">
+          <Switch>
+            <Route exact path="/" component={Login} />
+            <Route exact path="/sign-in" component={Login} />
+            <Route exact path="/sign-up" component={Register} />
+          </Switch>
+        </div>
+      </Router>
     );
   }
 }
