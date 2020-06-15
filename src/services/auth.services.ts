0efc4da705e355b0bed486f27850cd321c7dc63e
Aigars <39792018+AigarsPeda@users.noreply.github.com>
2020-06-15 09:59:15 +0300

    signUpUser and signInUser functions

---
+++ src/services/auth.services.ts
@@ -0,0 +1,27 @@
+import axios from "axios";
+import { IUser } from "../interfaces/interfaces";
+
+const API_ENDPOINT = "http://localhost:500";
+const config = {
+  headers: {
+    "Content-Type": "application/json",
+  },
+};
+
+export const signUpUser = async (userData: IUser) => {
+  const response = await axios.post(
+    `${API_ENDPOINT}/register`,
+    userData,
+    config
+  );
+  return response;
+};
+
+export const singInUser = async (userData: IUser) => {
+  const response = await axios.post(
+    `${API_ENDPOINT}/register`,
+    userData,
+    config
+  );
+  return response;
+};
