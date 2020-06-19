db83677c342116545e114f749ecd1585387a62e6
Aigars <39792018+AigarsPeda@users.noreply.github.com>
2020-06-19 12:47:41 +0300

    Box component

---
+++ src/components/main/card/Box.tsx
@@ -0,0 +1,25 @@
+import React from "react";
+
+interface Props {
+  cardValue: string;
+  title: string;
+  iconClass: string;
+  cardValueClass: string;
+}
+
+const Box: React.FC<Props> = (props) => {
+  const { cardValue, title, iconClass, cardValueClass } = props;
+  return (
+    <div className="col-md-6 col-xl-4">
+      <div className="card-box border">
+        <i className={`${iconClass} font-24`}></i>
+        <h3 className={cardValueClass}>{cardValue}</h3>
+        <p className="text-uppercase font-13 font-weight-medium mb-1">
+          {title}
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default Box;
---
+++ src/components/main/card/Card.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./Card.scss";
+
+const Card: React.FC = () => {
+  return (
+    <div>
+      <h1>HA</h1>
+    </div>
+  );
+};
+
+export default Card;
