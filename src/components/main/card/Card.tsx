be85833b424b59ae573bb320933be73feb55d7ab
Aigars <39792018+AigarsPeda@users.noreply.github.com>
2020-06-19 12:53:42 +0300

    Update Card.tsx

--- src/components/main/card/Card.tsx
+++ src/components/main/card/Card.tsx
@@ -1,10 +1,17 @@
 import React from "react";
 import "./Card.scss";
 
+import Box from "./Box";
+
 const Card: React.FC = () => {
   return (
     <div>
-      <h1>HA</h1>
+      <button className="btn btn-primary btn-add">Add Ticket</button>
+      <div className="text-center mb-2">
+        <div className="row">
+          <Box title="Total Ticket" cardValue="100" iconClass="fas fa-tag" />
+        </div>
+      </div>
     </div>
   );
 };
