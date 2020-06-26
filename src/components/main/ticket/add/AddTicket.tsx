c02e03df7210422625b38bc4bd5af77dea8fbddd
Aigars <39792018+AigarsPeda@users.noreply.github.com>
2020-06-26 09:05:39 +0300

    AddTicketForm component

---
+++ src/components/main/ticket/add/AddTicket.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Modal from "../../../reusable/modal/Modal";
+
+const AddTicket: React.FC = () => {
+  return (
+    <>
+      <Modal />
+    </>
+  );
+};
+
+export default AddTicket;
---
+++ src/components/main/ticket/add/AddTicketForm.tsx
@@ -0,0 +1,99 @@
+import React from "react";
+
+import FormInput from "../../../reusable/FormInput";
+import Button from "../../../reusable/Button";
+
+const AddTicketForm: React.FC = () => {
+  const onChange = () => {
+    console.log("click");
+  };
+  return (
+    <form>
+      <div className="form-group">
+        <FormInput
+          type="text"
+          name="fullname"
+          label="Full Name"
+          className="form-control"
+          placeholder="Enter Full Name"
+          value=""
+          error=""
+          onChange={onChange}
+        />
+      </div>
+      <div className="form-group">
+        <FormInput
+          type="text"
+          name="email"
+          label="Email"
+          className="form-control"
+          placeholder="Enter Email"
+          value=""
+          error=""
+          onChange={onChange}
+        />
+      </div>
+      <div className="form-group">
+        <FormInput
+          type="text"
+          name="email"
+          label="Departments"
+          className="form-control"
+          placeholder="Enter Email"
+          value=""
+          error=""
+          onChange={onChange}
+        />
+      </div>
+      <div className="form-group">
+        <FormInput
+          type="text"
+          name="email"
+          label="Priority"
+          className="form-control"
+          placeholder="Enter Priority"
+          value=""
+          error=""
+          onChange={onChange}
+        />
+      </div>
+      <div className="form-group">
+        <FormInput
+          type="text"
+          name="email"
+          label="Subject"
+          className="form-control"
+          placeholder="Enter Subject"
+          value=""
+          error=""
+          onChange={onChange}
+        />
+      </div>
+      <div className="form-group">
+        <label>Description</label>
+        <textarea
+          className="form-control"
+          name="description"
+          value=""
+          rows={5}
+          cols={40}
+          onChange={onChange}
+        />
+      </div>
+      <Button
+        className="btn btn-primary"
+        type="button"
+        label="ADD"
+        handleClick={onChange}
+      />
+      <Button
+        className="btn btn-danger"
+        type="button"
+        label="CANCEL"
+        handleClick={onChange}
+      />
+    </form>
+  );
+};
+
+export default AddTicketForm;
