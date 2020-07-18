f06e835123e29d48b391f7c2f694448f45e19c6f
Aigars Peda <aigarspeda@gmail.com>
2020-07-19 01:11:19 +0300

    Update authToken.ts

--- src/helpers/authToken.ts
+++ src/helpers/authToken.ts
@@ -2,9 +2,9 @@ import axios from "axios";
 
 const authToken = (token: string) => {
   if (token) {
-    axios.defaults.headers.common["Authorization"] = token;
+    axios.defaults.headers.common["authorization"] = token;
   } else {
-    delete axios.defaults.headers.common["Authorization"];
+    delete axios.defaults.headers.common["authorization"];
   }
 };
 
