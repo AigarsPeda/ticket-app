41ce40601fbff6342435c20853bf6228a0fbd157
Aigars Peda <aigarspeda@gmail.com>
2020-07-21 12:01:47 +0300

    Update modal.ts

--- src/redux/actions/modal.ts
+++ src/redux/actions/modal.ts
@@ -1,3 +1,15 @@
 import { SHOW_ADD_MODAL, SHOW_EDIT_MODAL } from "./../types";
 
-export const addModal = (payload: boolean) => (dispatch: any) => {};
+export const addModal = (payload: boolean) => (dispatch: any) => {
+  dispatch({
+    type: SHOW_ADD_MODAL,
+    payload: payload
+  });
+};
+
+export const editModal = (payload: boolean) => (dispatch: any) => {
+  dispatch({
+    type: SHOW_EDIT_MODAL,
+    payload: payload
+  });
+};
