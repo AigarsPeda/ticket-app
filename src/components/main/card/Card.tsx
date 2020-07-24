05f6363c14c5aa11a36796bfdbde19ce6fdc5331
Aigars Peda <aigarspeda@gmail.com>
2020-07-24 10:02:51 +0300

    Update Card.tsx

--- src/components/main/card/Card.tsx
+++ src/components/main/card/Card.tsx
@@ -1,17 +1,27 @@
 import React from "react";
+import { connect } from "react-redux";
+import _ from "lodash";
+
 import "./Card.scss";
 
 import Box from "./Box";
 import Button from "../../reusable/Button";
 
 import { addModal } from "../../../redux/actions/modal";
-import { connect } from "react-redux";
 import { RootState } from "../../../redux/reducers";
 
 type Props = ReturnType<typeof mapStateToProps> & typeof mapDispatchToProps;
 
 const Card: React.FC<Props> = (props) => {
   const { addModal, tickets } = props;
+
+  const findByStatus = (values: string) => {
+    return _.filter(tickets, ["status", values]).length;
+  };
+
+  const findByPriority = (values: string) => {
+    return _.filter(tickets, ["priority", values]).length;
+  };
 
   return (
     <>
@@ -23,15 +33,41 @@ const Card: React.FC<Props> = (props) => {
       />
       <div className="text-center mb-2">
         <div className="row">
-          {tickets.map((ticket) => {
-            return (
-              <Box
-                title={ticket.subject}
-                cardValue={ticket.priority}
-                iconClass="fas fa-tag"
-              />
-            );
-          })}
+          <Box
+            title="Total Tickets"
+            cardValue={tickets.length}
+            iconClass="fas fa-tag"
+          />
+          <Box
+            title="Open Tickets"
+            cardValue={findByStatus("Open")}
+            iconClass="fas fa-shield-alt"
+            cardValueClass="text-success"
+          />
+          <Box
+            title="Open Close"
+            cardValue={findByStatus("Close")}
+            iconClass="fas fa-shield-alt"
+            cardValueClass="text-muted"
+          />
+          <Box
+            title="High Priority Tickets"
+            cardValue={findByPriority("High")}
+            iconClass="fas fa-temperature-high"
+            cardValueClass="text-danger"
+          />
+          <Box
+            title="Medium Priority Tickets"
+            cardValue={findByPriority("Medium")}
+            iconClass="fas fa-folder-minus"
+            cardValueClass="text-warning"
+          />
+          <Box
+            title="Low Priority Tickets"
+            cardValue={findByPriority("Low")}
+            iconClass="fas fa-battery-quarter"
+            cardValueClass="text-muted"
+          />
         </div>
       </div>
     </>
