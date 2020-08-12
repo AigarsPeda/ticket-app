011c9b9a5726a9b78b0e30f30a59bae3384224e5
Aigars Peda <aigarspeda@gmail.com>
2020-08-12 07:18:11 +0300

    Table test with Enzyme

--- src/components/main/table-elements/table/__test__/Table.test.tsx
+++ src/components/main/table-elements/table/__test__/Table.test.tsx
@@ -10,10 +10,23 @@ import { render } from "@testing-library/react";
 import "@testing-library/jest-dom/extend-expect";
 
 import Table from "../Table";
-// import Enzyme, { shallow } from "enzyme";
-// import Adapter from "enzyme-adapter-react-16";
+import Enzyme, { mount } from "enzyme";
+import Adapter from "enzyme-adapter-react-16";
+Enzyme.configure({ adapter: new Adapter() });
 
-// Enzyme.configure({ adapter: new Adapter() });
+const ticketDataArray = [
+  {
+    fullName: "Aigars",
+    email: "aigarspeda@gmail.com",
+    subject: "Test",
+    description: "test",
+    department: "IT",
+    priority: "Low",
+    status: "Open",
+    tickedId: "12334354454",
+    _id: "121132334545465"
+  }
+];
 
 let state: any;
 let mockStore: any;
@@ -26,7 +39,7 @@ beforeEach(() => {
       isAuthenticated: true
     },
     tickets: {
-      tickets: []
+      tickets: ticketDataArray
     },
     entries: {
       entries: 5
@@ -57,19 +70,20 @@ describe("<Table />", () => {
     expect(getByTestId("tableComponent")).toHaveTextContent("Subject");
   });
 
-  // it("testing", () => {
-  //   const container = shallow(
-  //     <Provider store={store}>
-  //       <Table />
-  //     </Provider>
-  //   ).dive();
-
-  //   const table = container.find("table");
-  //   console.log(table);
-  //   const thead = table.find("thead");
-  //   // console.log(container.html());
-  //   expect(thead).toHaveLength(1);
-  // });
+  it("testing table", () => {
+    const container = mount(
+      <Provider store={store}>
+        <Table />
+      </Provider>
+    );
+    // console.log(container.debug());
+    expect(container).toHaveLength(1);
+    expect(container.find("table")).toHaveLength(1);
+    expect(container.find("tbody")).toHaveLength(1);
+    expect(container.find("th")).toHaveLength(8);
+    const tbody = container.find("tbody");
+    expect(tbody.find("tr")).toHaveLength(ticketDataArray.length);
+  });
 
   it("matches snapshot", () => {
     const tree = renderer
